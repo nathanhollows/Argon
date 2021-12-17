7b0ba594511998ec4d64aa75821cb0c7fb4ccc21
nathanhollows <me@nathanhollows.com>
2021-12-17 19:16:38 +1300

    Direct to scan event page

--- internal/handler/public/qr.go
+++ internal/handler/public/qr.go
@@ -16,7 +16,7 @@ import (
 func QR(env *handler.Env, w http.ResponseWriter, r *http.Request) error {
 	w.Header().Set("Content-Type", "image/svg+xml")
 	w.Header().Set("Cache-Control", "max-age=36000;")
-	path := fmt.Sprint(chi.URLParam(r, "code"))
+	path := fmt.Sprint("s/", chi.URLParam(r, "code"))
 	svgQR(helpers.URL(path), w)
 
 	return nil
